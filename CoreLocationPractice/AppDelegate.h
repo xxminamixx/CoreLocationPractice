c73d3e215e9e36cfafcf621564b5b7e692adc102
南　京兵 <k_minami@waku-2.com>
2016-08-19 10:04:07 +0900

    最初のコミット

---
+++ CoreLocationPractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreLocationPractice
+//
+//  Created by 南　京兵 on 2016/08/19.
+//  Copyright © 2016年 南　京兵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreLocationPractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreLocationPractice
+//
+//  Created by 南　京兵 on 2016/08/19.
+//  Copyright © 2016年 南　京兵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
